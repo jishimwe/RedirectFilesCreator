1e122a672bf45d227f2cddb99ee10950f61116be
Jean-Paul Ishimwe <jean-paul.ishimwe@student.uclouvain.be>
2022-09-27 17:45:57 +0200

    First commit

--- RedirectFilesCreator/GitExplorer.cs
+++ RedirectFilesCreator/GitExplorer.cs
@@ -13,10 +13,15 @@ namespace RedirectFilesCreator
         private const string pathToRedirectRepo = @"This PC\\Documents\\Redirect";
         private const string testRepo = "https://github.com/jishimwe/PlayMusic.git";
 
+        // Constants for filenaming
+        private const string RdrExt = ".redir";
+
         public static string PathToRepo => pathToRepo;
         public static string PathToRedirectRepo => pathToRedirectRepo;
         public static string TestRepo => testRepo;
 
+
+        // Might be useless
         public static bool ExploreRepo(string repo )
         {
             if (repo == null) return false;
@@ -27,6 +32,57 @@ namespace RedirectFilesCreator
             Repository.Clone(TestRepo, PathToRepo, co);
 
             return true;
-        }  
+        }
+
+        public static bool CreateRedirectRepo(string  origPath, string destPath)
+        {
+            try
+            {
+                // Verifying if the source code folder exists
+                DirectoryInfo origInfo = new DirectoryInfo(origPath);
+                if (!origInfo.Exists) return false;
+
+                // Verifying if the destination exists and deleting it in the affiramtive and creating a new one
+                DirectoryInfo destInfo = new DirectoryInfo(destPath);
+                if (destInfo.Exists)
+                {
+                    destInfo.Delete();
+                }
+                destInfo.Create();
+
+                // Running DirectoryWalk and creating redir files
+
+                return DirectoryWalk(origInfo, destInfo);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return false;
+        }
+
+        public static bool DirectoryWalk(DirectoryInfo origDir, DirectoryInfo destDir)
+        {
+            FileInfo[] files = null;
+            DirectoryInfo dirs = null;
+            try
+            {
+                files = origDir.GetFiles("*.*");
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            foreach (FileInfo fi in files {
+                string redirFilename = fi.Name + RdrExt;
+                string redirFilepath = Path.Combine(destDir.FullName, redirFilename);
+                using (FileStream fs = File.Create(redirFilepath))
+                {
+                    fs.Write(fi.FullPath);
+                }
+            }
+
+            return false;
+        }
     }
 }
