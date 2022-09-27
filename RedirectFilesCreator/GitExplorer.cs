0cf208daa8ca322f38d7742c21bff38dc6bbcc02
Jean-Paul Ishimwe <jean-paul.ishimwe@student.uclouvain.be>
2022-09-27 16:49:18 +0200

    First commit

---
+++ RedirectFilesCreator/GitExplorer.cs
@@ -0,0 +1,32 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedirectFilesCreator
+{
+    public static class GitExplorer
+    {
+        private const string pathToRepo = @"This PC\Documents\Repo";
+        private const string pathToRedirectRepo = @"This PC\\Documents\\Redirect";
+        private const string testRepo = "https://github.com/jishimwe/PlayMusic.git";
+
+        public static string PathToRepo => pathToRepo;
+        public static string PathToRedirectRepo => pathToRedirectRepo;
+        public static string TestRepo => testRepo;
+
+        public static bool ExploreRepo(string repo )
+        {
+            if (repo == null) return false;
+            var co = new CloneOptions();
+
+            // TODO: Ask for credentials to avoid hardcoded password
+            co.CredentialsProvider = (_url, _user, _cred) => new UsernamePasswordCredentials { Username = "jpishimwe", Password = "ISHjp1160" };
+            Repository.Clone(TestRepo, PathToRepo, co);
+
+            return true;
+        }  
+    }
+}
---
+++ RedirectFilesCreator/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RedirectFilesCreator;
+
+Console.WriteLine("Cloning Repo from {0} to {1}", GitExplorer.TestRepo, GitExplorer.PathToRepo);
+if (GitExplorer.ExploreRepo(GitExplorer.PathToRepo))
+{
+    Console.WriteLine("Repo Cloned");
+} else
+{
+    Console.WriteLine("Error Cloning Repo");
+}
