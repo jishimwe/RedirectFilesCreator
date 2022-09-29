9bb765eec61da2f427bd0988518283b0a78ee66c
unknown <jeanpaulishimwe@gmail.com>
2022-09-29 14:37:05 +0200

    git file path name

--- RedirectFilesCreator/GitExplorer.cs
+++ RedirectFilesCreator/GitExplorer.cs
@@ -43,7 +43,7 @@ namespace RedirectFilesCreator
                 DirectoryInfo origInfo = new DirectoryInfo(origPath);
                 if (!origInfo.Exists) return false;
 
-                // Verifying if the destination exists and deleting it in the affiramtive and creating a new one
+                // Verifying if the destination exists and deleting it in the affirmative and creating a new one
                 DirectoryInfo destInfo = new DirectoryInfo(destPath);
                 if (destInfo.Exists)
                 {
@@ -62,7 +62,7 @@ namespace RedirectFilesCreator
             return false;
         }
 
-        public static bool DirectoryWalk(DirectoryInfo origDir, DirectoryInfo destDir)
+        public static bool DirectoryWalk(DirectoryInfo origDir, DirectoryInfo destDir, string pathSoFar = null)
         {
             FileInfo[] files = null;
             DirectoryInfo[] dirs = null;
@@ -78,10 +78,15 @@ namespace RedirectFilesCreator
             {
                 string redirFilename = fi.Name + RdrExt;
                 string redirFilepath = Path.Combine(destDir.FullName, redirFilename);
+                string gitPath = (pathSoFar != null) ? pathSoFar + "\\" + fi.Name : fi.Name;
+                Console.WriteLine(gitPath);
                 using (FileStream fs = File.Create(redirFilepath))
                 {
-                    byte[] data = new UTF8Encoding(true).GetBytes(fi.FullName);
-                    fs.Write(data);
+                    //byte[] data = new UTF8Encoding(true).GetBytes(fi.FullName);
+                    //fs.Write(data);
+                    byte[] buff = new UTF8Encoding(true).GetBytes(gitPath);
+                    fs.Write(buff);
+                    fs.Close();
                 }
             }
 
@@ -92,7 +97,9 @@ namespace RedirectFilesCreator
                 {
                     string redirPath = destDir.FullName + "\\" + di.Name;
                     Directory.CreateDirectory(redirPath);
-                    if (!DirectoryWalk(di, new DirectoryInfo(redirPath))) return false;
+                    pathSoFar = (pathSoFar != null) ? pathSoFar + "\\" + di.Name : di.Name;
+                    //Console.WriteLine(pathSoFar);
+                    if (!DirectoryWalk(di, new DirectoryInfo(redirPath), pathSoFar)) return false;
                 }
             } catch (Exception e)
             {
