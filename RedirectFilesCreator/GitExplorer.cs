709e90c55032d30ea51d845cbdc31c59d2edece6
jpishimwe <jeanpaulishimwe@gmail.com>
2023-05-02 10:31:21 +0200

    add -v command line argument and streamlining

--- RedirectFilesCreator/GitExplorer.cs
+++ RedirectFilesCreator/GitExplorer.cs
@@ -1,49 +1,18 @@
-﻿using LibGit2Sharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace RedirectFilesCreator
 {
-
-    // TODO: Git integration
-    public static class GitExplorer
+	public static class GitExplorer
     {
-        private const string pathToRepo = @"C:\Users\ishim\Documents\Repo";
-        private const string pathToRedirectRepo = @"C:\Users\ishim\Documents\Redirect";
-        private const string testRepo = "https://github.com/jishimwe/PlayMusic.git";
-
         // Constants for filenaming
         private const string RdrExt = ".redir";
 
-        public static string PathToRepo => pathToRepo;
-        public static string PathToRedirectRepo => pathToRedirectRepo; 
-        public static string TestRepo => testRepo;
-
-
-        // Might be useless
-        public static bool ExploreRepo(string repo )
-        {
-            if (repo == null) return false;
-            var co = new CloneOptions();
-
-            // TODO: Ask for credentials to avoid hardcoded password
-            co.CredentialsProvider = (_url, _user, _cred) => new UsernamePasswordCredentials { Username = "jpishimwe", Password = "" };
-            Repository.Clone(TestRepo, PathToRepo, co);
-
-            return true;
-        }
-
         // Open a redirect file and check if the file redirected to exist
-        public static bool RedirectToFile(string redirPath, string realPathRoot = pathToRepo)
+        public static bool RedirectToFile(string redirPath, string realPathRoot)
         {
             using(StreamReader file = new StreamReader(redirPath))
             {
-                string ln = file.ReadLine();
-                //string realPath = realPathRoot + "\\" + ln;
+                string? ln = file.ReadLine();
                 if (ln == null || !File.Exists(realPathRoot + "\\" + ln)) 
                     return false;
                 return true;
@@ -53,27 +22,19 @@ namespace RedirectFilesCreator
         public static bool VerifyRedirectIntegrity(string pathToRedir, string repoRoot)
         {
             DirectoryInfo di = new DirectoryInfo(pathToRedir);
-            FileInfo[] files = null;
-            DirectoryInfo[] dirs = null;
 
             try
             {
-                files = di.GetFiles("*.*");
+				FileInfo[] files = di.GetFiles("*.*");
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
-            
-            foreach (FileInfo fi in files)
-            {
-                if (!RedirectToFile(fi.FullName, repoRoot)) return false;
-                Console.Write("._ ");
-            }
 
             try
             {
-                dirs = di.GetDirectories();
+				DirectoryInfo[] dirs = di.GetDirectories();
                 foreach (DirectoryInfo dir in dirs)
                 {
                     string path = dir.FullName;
@@ -115,10 +76,9 @@ namespace RedirectFilesCreator
             return false;
         }
 
-        public static bool DirectoryWalk(DirectoryInfo origDir, DirectoryInfo destDir, string pathSoFar = null)
+        public static bool DirectoryWalk(DirectoryInfo origDir, DirectoryInfo destDir, string? pathSoFar = null)
         {
-            FileInfo[] files = null;
-            DirectoryInfo[] dirs = null;
+            FileInfo[]? files = null;
             try
             {
                 files = origDir.GetFiles("*.*");
@@ -127,7 +87,7 @@ namespace RedirectFilesCreator
                 Console.WriteLine(e.Message);
             }
 
-            foreach (FileInfo fi in files) 
+			foreach (FileInfo fi in files) 
             {
                 string redirFilename = fi.Name + RdrExt;
                 string redirFilepath = Path.Combine(destDir.FullName, redirFilename);
@@ -135,8 +95,6 @@ namespace RedirectFilesCreator
                 Console.WriteLine(gitPath);
                 using (FileStream fs = File.Create(redirFilepath))
                 {
-                    //byte[] data = new UTF8Encoding(true).GetBytes(fi.FullName);
-                    //fs.Write(data);
                     byte[] buff = new UTF8Encoding(true).GetBytes(gitPath);
                     fs.Write(buff);
                     fs.Close();
@@ -145,13 +103,12 @@ namespace RedirectFilesCreator
 
             try
             {
-                dirs = origDir.GetDirectories();
+				DirectoryInfo[] dirs = origDir.GetDirectories();
                 foreach (DirectoryInfo di in dirs)
                 {
                     string redirPath = destDir.FullName + "\\" + di.Name;
                     Directory.CreateDirectory(redirPath);
                     string thisPath = (pathSoFar != null) ? pathSoFar + "\\" + di.Name : di.Name;
-                    //Console.WriteLine(pathSoFar);
                     if (!DirectoryWalk(di, new DirectoryInfo(redirPath), thisPath)) return false;
                 }
             } catch (Exception e)
--- RedirectFilesCreator/Program.cs
+++ RedirectFilesCreator/Program.cs
@@ -9,17 +9,17 @@ class Program
      */
     static void Main(string[] args)
     {
-        if (args == null)
+        if (args == null || args.Length == 0)
         {
-            Console.WriteLine("No argument. Launching tests instances");
-            DefaultExec();
+            PrintUsage();
+            Environment.Exit(-2);
         }
-        if (args.Length == 0) DefaultExec();
 
         Console.WriteLine(args.ToString());
         Array.ForEach(args, Console.WriteLine);
-        string sourceRepo = null;
-        string targetRepo = null;
+        string? sourceRepo = null;
+        string? targetRepo = null;
+        bool isValid = false;
         for (int i = 0; i < args.Length; i += 2)
         {
             string s = args[i];
@@ -33,28 +33,37 @@ class Program
                     targetRepo = args[i + 1];
                     break;
 
-                default:
-                    sourceRepo = GitExplorer.PathToRepo;
-                    targetRepo = GitExplorer.PathToRedirectRepo;
+                case "-verify":
+                case "-v":
+                    isValid = true;
                     break;
+
+                default:
+                    PrintUsage();
+					Environment.Exit(-2);
+					break;
             }
         }
         ResultCreateRedirectRepo(GitExplorer.CreateRedirectRepo(sourceRepo, targetRepo));
-        if (GitExplorer.VerifyRedirectIntegrity(targetRepo, sourceRepo))
+		Console.BackgroundColor = ConsoleColor.Green;
+		if (isValid && GitExplorer.VerifyRedirectIntegrity(targetRepo, sourceRepo))
             Console.WriteLine("Redirect repo is valid");
-        else
+        else if (isValid)
             Console.WriteLine("Redirect repos is invalid");
-    }
-
-    static void DefaultExec()
-    { 
-        Console.WriteLine("Creating Redirect Repo ... \n [Source {0}] \t [Redirect {1}]", GitExplorer.PathToRepo, GitExplorer.PathToRedirectRepo);
-         ResultCreateRedirectRepo(GitExplorer.CreateRedirectRepo(GitExplorer.PathToRepo, GitExplorer.PathToRedirectRepo));
-    }
+		Console.ResetColor();
+	}
 
     static void ResultCreateRedirectRepo(bool res)
     {
         string message = res ? "Redirect Repo Created" : "Error Creating Redirect Repo";
         Console.WriteLine(message);
+    }
+
+    static void PrintUsage()
+    {
+        Console.WriteLine("Invalid arguments\n" +
+            "Launch the program with the following arguments:\n" +
+            "\t -s <path>\t: path to the source code root folder\n" +
+            "\t -d <path>\t: path to the folder that will contain the redirect files");
     }
 }
